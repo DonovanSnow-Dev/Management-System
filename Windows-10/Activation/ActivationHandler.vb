6a0b1ba9922029f5b7935f24058e3824ba871789
Donovan N Snow <93895594+DonovanSnow-Dev@users.noreply.github.com>
2022-02-05 04:59:49 +0200

    Added File: ActivationHandler.vb

---
+++ Windows-10/Activation/ActivationHandler.vb
@@ -0,0 +1,45 @@
+Namespace Activation
+
+    Friend MustInherit
+
+    Class ActivationHandler
+
+        Public MustOverride
+
+        Function CanHandle(args As Object) As Boolean
+
+            Public MustOverride
+
+        Function HandleAsync(args As Object) As Task
+
+    End Class
+
+    Friend MustInherit
+
+    Class ActivationHandler(Of T As Class)
+
+        Inherits ActivationHandler
+
+        Protected MustOverride
+
+        Function HandleInternalAsync(args As T) As Task
+
+        Public Overrides Async Function HandleAsync(args As Object) As Task
+            Await HandleInternalAsync(TryCast(args, T))
+        End Function
+
+        Public Overrides
+
+        Function CanHandle(args As Object) As Boolean
+            Return TypeOf args Is T AndAlso CanHandleInternal(TryCast(args, T))
+        End Function
+
+        Protected Overridable
+
+        Function CanHandleInternal(args As T) As Boolean
+            Return True
+        End Function
+
+    End Class
+
+End Namespace
